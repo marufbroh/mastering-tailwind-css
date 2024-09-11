cf4fe11d398f2ecf037e4bce51343b5fcd22a0d1
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-11 12:19:18 +0600

    Tailwind Configurations - Advanced Concepts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
+  darkMode: "media",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myColor: {
+          400: "#1fb6ff",
+        },
+      },
+    },
   },
   plugins: [],
-}
+  
+};
