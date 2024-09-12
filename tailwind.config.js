18159f34050527ac3b5dba22e99373768704e928
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-12 10:47:40 +0600

    Extend Theme - Advanced Concepts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,26 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   darkMode: "media",
   theme: {
-    screens: {
-      sm: "640px",
-      lg: "1024px",
-      desktop: "2560px",
+    // screens: {
+    //   sm: "640px",
+    //   lg: "1024px",
+    //   desktop: "2560px",
+    // },
+    colors: {
+      blue: {
+        light: "f4f4f4",
+        DEFAULT: "f4f4f3",
+        dark: "f4f4f5",
+      },
+      // it means other colors will be gone
     },
-    extend: {},
+    extend: {
+      screens: {
+        sm: "640px",
+        lg: "1024px",
+        desktop: "2560px",
+      },
+    },
   },
   plugins: [],
 };
