44f6e98527fe907554c665496ad819376fcbf176
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-14 09:13:36 +0600

    Pseudo classes and elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html"],
-  darkMode: "media",
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    // screens: {
-    //   sm: "640px",
-    //   lg: "1024px",
-    //   desktop: "2560px",
-    // },
-    colors: {
-      blue: {
-        light: "f4f4f4",
-        DEFAULT: "f4f4f3",
-        dark: "f4f4f5",
-      },
-      // it means other colors will be gone
-    },
-    extend: {
-      screens: {
-        sm: "640px",
-        lg: "1024px",
-        desktop: "2560px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
